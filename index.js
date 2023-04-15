0fcf0f44097f59a2c7e450e57c1102be952edec1
shreyash19-pixel <shreyashp734@gmail.com>
2023-04-15 14:21:53 +0530

    Created a new interior labs landing page

---
+++ index.js
@@ -0,0 +1,44 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    nav: false,
+    dots:false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    },autoplay:true,
+    autoplayTimeout:1500,
+    autoplayHoverPause:true
+    });
+    $('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1500])
+    })
+    $('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+    })
+
+
+const bar = document.querySelector(".bar");
+
+const nav_links = document.querySelector(".nav-links"); 
+const ham = document.querySelector(".hamburger");
+const cross = document.querySelector(".cross");
+
+bar.addEventListener("click",()=>{
+    
+    nav_links.classList.toggle("active");
+    ham.classList.toggle("inactive");
+    cross.classList.toggle("active");
+})
+
+cross.addEventListener("click",()=>{
+    nav_links.classList.remove("active");
+    ham.classList.remove("inactive");
+    cross.classList.remove("active");
+})
